88eb8fe87e84018720ae8dd63245d6199ab9003a
a13v3r <verbitskiy_al@mail.ru>
2024-10-07 19:12:17 +0300

    е

---
+++ UserService/Repositories/DbConnection.cs
@@ -0,0 +1,6 @@
+﻿namespace UserService.Repositories;
+
+public class DbConnectionString : IDbConnectionString
+{
+    public string Connst { get; set; } = "Host=127.0.0.1;Database=postgres;Username=postgres;Port=5432;Password=";
+}
